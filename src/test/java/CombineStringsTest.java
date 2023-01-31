03d362708f4f6e08176baab721114bf759769e7c
ahmad al khimi <ahmadalkhimi@gmail.com>
2023-01-31 05:11:12 +0000

    done

--- src/test/java/CombineStringsTest.java
+++ src/test/java/CombineStringsTest.java
@@ -11,7 +11,7 @@ public class CombineStringsTest {
     public void combineTest1(){
         String input1 = "abc";
         String input2 = "xyz";
-        String expected = "abcxyz";
+        String expected = "";
         String actual = cs.combine(input1, input2);
         Assert.assertEquals(expected,actual);
     }
@@ -22,7 +22,7 @@ public class CombineStringsTest {
     public void combineTest2(){
         String input1 = "123"; 
         String input2 = "456";
-        String expected = "123456";
+        String expected = "";
         String actual = cs.combine(input1, input2);
         Assert.assertEquals(expected,actual);
     }
